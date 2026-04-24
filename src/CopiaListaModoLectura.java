d8effc357b32e336c6574498d10240173581b5f7
Jona <jonix17@gmail.com>
2026-04-24 17:23:43 +0300

    Incluimos listas

---
+++ src/CopiaListaModoLectura.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class CopiaListaModoLectura {
+
+    private List<ModuloProfesional> modulos;
+
+    public List<ModuloProfesional> getModulos() {
+        return modulos;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    public static void main(String[] args) {
 
-    List<Contacto> agenda = new ArrayList<>();
+
+        String[] vector = new String[10];
+        List<String> lista = new ArrayList<>();
+
+        vector[0] = "HOLA";
+        vector[1] = "MUNDO";
+
+        lista.add("HOLA");
+        lista.add("MUNDO");
+
+        System.out.println(vector[0]);
+        System.out.println(lista.get(0));
+
+        vector[0] = "ADIÓS";
+        lista.set(0, "ADIÓS");
+
+        System.out.println(vector[0]);
+        System.out.println(lista.get(0));
+
+
+
+    }
 
 
 }
